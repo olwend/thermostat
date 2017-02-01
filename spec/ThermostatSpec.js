fce6cc23c2c52d34e33031af6d74d8ce24c85e16
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-02-01 14:58:58 +0000

    Power Saving Mode switched off

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,9 +8,8 @@ beforeEach(function() {
 
 describe('thermostat temperature', function() {
     it('should be set at 20 degrees', function() {
-        expect(thermostat._defaulttemp).toEqual(20)
-    })
-<<<<<<< HEAD
+            expect(thermostat._defaulttemp).toEqual(20)
+        })
 })
 
 describe('increase thermostat temperature', function() {
@@ -28,44 +27,25 @@ describe('decrease thermostat temperature', function() {
 })
 
 describe('throws error when minimum temp is reached', function() {
-    it('should throw error if minumum temp is reached', function() {
-      for (var i = thermostat._defaulttemp; i > thermostat._minimumtemp; i--) {
-            thermostat.decreaseTemp();
-        }
-        expect(function() {
-            thermostat.decreaseTemp()
-        }).toThrowError("minimum temperature has been reached");
-    })
-
-    it('has power saving mode on by default', function() {
-    expect(thermostat.isPowerSavingModeOn()).toBe(true);
-  });
-})
-=======
+        it('should throw error if minumum temp is reached', function() {
+            for (var i = thermostat._defaulttemp; i > thermostat._minimumtemp; i--) {
+                thermostat.decreaseTemp();
+            }
+            expect(function() {
+                thermostat.decreaseTemp()
+            }).toThrowError("minimum temperature has been reached");
+        })
 })
 
-describe('increase thermostat temperature', function() {
-    it('should increase temp', function() {
-        thermostat.increaseTemp();
-        expect(thermostat._defaulttemp).toEqual(21)
+describe('Sets power saving to default', function() {
+       it('has power saving mode on by default', function() {
+            expect(thermostat.isPowerSavingModeOn()).toBe(true);
+        })
     })
-})
 
-describe('decrease thermostat temperature', function() {
-    it('should decrease temp', function() {
-        thermostat.decreaseTemp();
-        expect(thermostat._defaulttemp).toEqual(19)
+describe('Power Saving Mode is switched off', function() {
+    it('Switches off Power Saving Mode', function() {
+      thermostat.isPowerSavingModeOff();
+      expect(thermostat.isPowerSavingModeOn()).toBe(false);
     })
 })
-
-describe('throws error when minimum temp is reached', function() {
-    it('should throw error if minumum temp is reached', function() {
-      for (var i = thermostat._defaulttemp; i > thermostat._minimumtemp; i--) {
-            thermostat.decreaseTemp();
-        }
-        expect(function() {
-            thermostat.decreaseTemp()
-        }).toThrowError("minimum temperature has been reached");
-    })
-})
->>>>>>> c689e639efc0445348ba7944bcf1d9bf948f7736
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,31 +1,29 @@
 'use strict'
 
-function Thermostat(){
-  this._defaulttemp = 20
-  this._minimumtemp = 10
-<<<<<<< HEAD
-  this.powerSavingMode = true
-=======
->>>>>>> c689e639efc0445348ba7944bcf1d9bf948f7736
+function Thermostat() {
+    this._defaulttemp = 20
+    this._minimumtemp = 10
+    this.powerSavingMode = true
+
+
 };
 
 Thermostat.prototype.increaseTemp = function() {
-  return this._defaulttemp += 1;
+    return this._defaulttemp += 1;
 }
 
-Thermostat.prototype.decreaseTemp = function(){
-  if (this._defaulttemp <= this._minimumtemp){
-    throw new Error("minimum temperature has been reached")
-  }else{
-    return this._defaulttemp -= 1;
-  }
-
-<<<<<<< HEAD
+Thermostat.prototype.decreaseTemp = function() {
+    if (this._defaulttemp <= this._minimumtemp) {
+        throw new Error("minimum temperature has been reached")
+    } else {
+        return this._defaulttemp -= 1;
+    }
 }
 
-Thermostat.prototype.isPowerSavingModeOn = function(){
-  return this.powerSavingMode === true;
+Thermostat.prototype.isPowerSavingModeOn = function() {
+    return this.powerSavingMode === true;
+}
 
-=======
->>>>>>> c689e639efc0445348ba7944bcf1d9bf948f7736
+Thermostat.prototype.isPowerSavingModeOff = function() {
+    return this.powerSavingMode = false;
 }
