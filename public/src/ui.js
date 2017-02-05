2bcf792b67f7475dd719de4f676bbd0d84fae785
olwend <olwend@gmail.com>
2017-02-05 09:50:26 +0000

    OpenWeather API AJAX integration

--- public/src/ui.js
+++ public/src/ui.js
@@ -5,6 +5,22 @@ $(document).ready(function() {
        updateTemperature();
        updatePowerSaveStatus();
 
+       function displayWeather(city) {
+         var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+         var token = '&appid=69638d45e969469d756d603cbf4447e9';
+         var units = '&units=metric';
+         $.get(url + token + units, function(data) {
+           $('#current-temperature').text(data.main.temp + ' degrees celsius ');
+         });
+       };
+
+       displayWeather('Oakham');
+
+       $('#select-city').submit(function(event) {
+         event.preventDefault();
+         var city = $('#current-city').val();
+         displayWeather(city);
+       });
 
 
        $('#temp-down').click(function(event) {
---
+++ src/Thermostat.js
@@ -0,0 +1,75 @@
+'use strict';
+
+function Thermostat() {
+  this.temperature = 20;
+  this.MIN_TEMP = 10;
+  this.MAX_TEMP_PSM_ON = 25;
+  this.MAX_TEMP_PSM_OFF = 32;
+  this.powerSavingMode = true;
+  this.DEFAULT_TEMPERATURE = 20;
+  this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+}
+
+Thermostat.prototype.getCurrentTemperature = function(){
+  return this.temperature;
+};
+
+Thermostat.prototype.up = function(){
+  if (this.isMaxTemp()){
+    return;
+  }
+  this.temperature += 1;
+  console.log(this.temperature);
+};
+
+Thermostat.prototype.down = function(){
+  if (this.isMinTemp()){
+    return;
+  }
+  this.temperature -= 1;
+};
+
+Thermostat.prototype.isMinTemp = function(){
+  return this.temperature === this.MIN_TEMP;
+}
+
+Thermostat.prototype.isPowerSavingModeOn = function(){
+  return this.powerSavingMode === true;
+}
+
+Thermostat.prototype.switchPowerSavingModeOff = function(){
+  this.powerSavingMode = false;
+}
+
+Thermostat.prototype.switchPowerSavingModeOn = function(){
+  this.powerSavingMode = true;
+}
+
+Thermostat.prototype.switchPSM = function(){
+  if (this.isPowerSavingModeOn()=== false) {
+    this.powerSavingMode = true;
+  } else {
+    this.powerSavingMode = false;
+  }
+}
+
+Thermostat.prototype.isMaxTemp = function() {
+  if (this.isPowerSavingModeOn()=== false) {
+    return this.temperature === this.MAX_TEMP_PSM_OFF;
+  }
+  return this.temperature === this.MAX_TEMP_PSM_ON;
+}
+
+Thermostat.prototype.reset = function(){
+  return this.temperature = this.DEFAULT_TEMPERATURE;
+}
+
+Thermostat.prototype.energyUsage = function() {
+  if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+    return 'low-usage';
+  }
+  if (this.temperature > this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature < this.MAX_TEMP_PSM_ON){
+    return 'medium-usage';
+  }
+   return 'high-usage';
+}
